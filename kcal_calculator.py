38dbb82edc1399eaf4087449d9ee069bc66df7bb
yigit-glitch91 <yigitmaral022@gmail.com>
2025-09-08 11:18:40 +0300

    kcal_calculator.py

--- kcal_calculator.py
+++ kcal_calculator.py
@@ -5,15 +5,16 @@ weight = float(input("Your weight: "))
 height = float(input("Your height: "))
 age = float(input("Your age: "))
 
-if(cinsiyet == "male"):{
+if(gender == "male"):{
     print("Your metabolism speed is: " + str(66.5 + (13.75 * weight) + (5 * height) - (6.77 * age)) + " kcal")
 }
     
-elif(cinsiyet == "female"):{
+elif(gender == "female"):{
     print("Your metabolism speed is:  " + str(655.1 + (9.56 * weight) + (1.85 * height) - (4.67 * age)) + " kcal")
 }
     
 else:{
     print("Something is wrong. Please try again...")
 }
+
 
